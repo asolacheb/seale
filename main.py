0c2b606be7d181a21a03e4e64073c5c5e0e72391
asolacheb <151604432+asolacheb@users.noreply.github.com>
2024-05-20 19:07:15 +0000

    Describe your changes here

--- main.py
+++ main.py
@@ -204,7 +204,6 @@ def main():
     )
     
     st.markdown('<img src="https://sealeai.streamlit.app/~/+/media/9d638b84659efdc8f416113e914223679845a0196fe192eb1ba2d400.png" class="logo-img">', unsafe_allow_html=True)    
-    st.image("assets/logo.png")
     st.markdown('<h1 class="primary-color">Seale Comp Finder</h1>', unsafe_allow_html=True)
     company_name = st.text_input("Enter a brief description:")
     
