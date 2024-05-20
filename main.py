98df77634322a8b5fe8937d95b2f2530b000b808
asolacheb <151604432+asolacheb@users.noreply.github.com>
2024-05-20 19:33:59 +0000

    Describe your changes here

--- main.py
+++ main.py
@@ -203,7 +203,7 @@ def main():
         unsafe_allow_html=True
     )
     
-    st.markdown('<img src="https://sealeai.streamlit.app/~/+/media/9d638b84659efdc8f416113e914223679845a0196fe192eb1ba2d400.png" class="logo-img">', unsafe_allow_html=True)    
+    st.markdown('<img src="/workspaces/seale/assets/logo.png" class="logo-img">', unsafe_allow_html=True)    
     st.markdown('<h1 class="primary-color">Seale Comp Finder</h1>', unsafe_allow_html=True)
     company_name = st.text_input("",placeholder="Enter a brief description")
     
